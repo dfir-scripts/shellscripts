200fd8a607b705b557fdaf56aa37140dbe83e513
JSBrown <siftgrab@users.noreply.github.com>
2014-09-10 08:22:32 -0500

    Create grabVThash.sh

---
+++ grabVThash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "GrabVThash.sh searches Virus Total DB for MD5 values"
+# jsbrown, CCajigas
+echo ""
+echo "Usage: grabVThash.sh [file or hash...]" 
+echo ""
+#process file
+[ -f "$1" ] && echo "Processing..." && cat $1|cut -c -32| while read line; do curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="---api-key---" --form resource="$line" |awk -F'positives\":' '{print "VT Hits:" $2}'|awk -F' ' '{print $1$2" "$3$6$7}'|sed 's/["}]//g' && sleep 15 ; done && exit
+#process single hash
+HTEST=$(echo $1|grep -e "[0-9a-f]\{32\}")
+[ ! -f "$1" ] && [ "$HTEST" != $1 ] && echo "$1 is not a valid md5 hash"
+[ ! -f "$1" ] && [ "$HTEST" == $1 ] && echo "Processing..."  && curl -s -X POST 'https://www.virustotal.com/vtapi/v2/file/report' --form apikey="---api-key---" --form resource="$1" |awk -F'positives\":' '{print "VT Hits:" $2}'|awk -F' ' '{print $1$2" "$3$6$7}'|sed 's/["}]//g' && exit
